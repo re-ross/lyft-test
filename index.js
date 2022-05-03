7d78a4e3abcb7d90d5211eef5fdb044b4710e3e4
Ryan Ross <ryan@reross.com>
2022-05-03 11:17:25 -0500

    able to hit /test and return JSON object successfully

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
 
-const cutString = (string) => {
-  let returnedString = "";
+const cutToString = (string) => {
+  let cutString = "";
   const characters = string.split("");
   for (let i = 2; i < characters.length; i += 3) {
-    returnedString += characters[i];
+    cutString += characters[i];
   }
-  return returnedString;
+  return { return_string: cutString };
 };
+
+app.post("/test", (req, res) => {
+  const { string_to_cut } = req.body;
+
+  if (string_to_cut && typeof string_to_cut === "string") {
+    res.status(200).send(cutToString(string_to_cut));
+  } else {
+    res.status(400).send("Please provide a string!");
+  }
+});
 
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
